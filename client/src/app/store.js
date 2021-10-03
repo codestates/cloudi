1f9ca732702dd4197cb6fbe471e43e94302f66cf
KimMinchan95 <hialscks@gmail.com>
2021-10-03 21:37:29 +0900

    Feat: Add localstorage

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,12 +1,30 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
+
 import stickReducer from './modules/stick';
 import standReducer from './modules/stand';
 import userinfoReducer from './modules/userinfo';
 
-export const store = configureStore({
-  reducer: {
-    stick: stickReducer,
-    stand: standReducer,
-    userinfo: userinfoReducer
-  }
+const reducers = combineReducers({
+  stick: stickReducer,
+  stand: standReducer,
+  userinfo: userinfoReducer
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  midddleware: [thunk],
+});
+
+export default store;
