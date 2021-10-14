5c54041881eb8e5fd66c6e414b408a180b381835
dankhan102 <skshim2002@gmail.com>
2021-10-14 23:38:52 +0900

    Fix: edit App.js didMount function logic

--- client/src/App.js
+++ client/src/App.js
@@ -33,6 +33,7 @@ const App = () => {
     const url = new URL(window.location.href);
     const google = url.searchParams.get('scope');
     const authorizationCode = url.searchParams.get('code');
+    setIsLoading(true);
     if (authorizationCode) {
       if (google) {
         axios({
@@ -40,56 +41,60 @@ const App = () => {
           url: 'https://www.cloudi.shop/user/google',
           data: { orders, code: authorizationCode }
         })
-          .then((res) => {
-            dispatch(
-              insertUserinfo({
-                id: res.data.id,
-                kakaoId: res.data.kakaoId,
-                googleId: res.data.googldId,
-                isAdmin: res.data.isAdmin,
-                userEmail: res.data.userEmail,
-                userName: res.data.userName,
-                token: res.data.token
-              })
-            );
-            dispatch(insertAllSticks(res.data.orders.sticks));
-            dispatch(insertAllStands(res.data.orders.stands));
-          })
-          .catch((err) => console.log('Google Login', err));
+        .then((res) => {
+          dispatch(
+            insertUserinfo({
+              id: res.data.id,
+              kakaoId: res.data.kakaoId,
+              googleId: res.data.googldId,
+              isAdmin: res.data.isAdmin,
+              userEmail: res.data.userEmail,
+              userName: res.data.userName,
+              token: res.data.token
+            })
+          );
+          dispatch(insertAllSticks(res.data.orders.sticks));
+          dispatch(insertAllStands(res.data.orders.stands));
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          console.log('Google Login', err);
+          setIsLoading(false);
+        });
       } else {
         axios({
           method: 'POST',
           url: 'https://www.cloudi.shop/user/kakao',
           data: { orders, code: authorizationCode }
         })
-          .then((res) => {
-            dispatch(
-              insertUserinfo({
-                id: res.data.id,
-                kakaoId: res.data.kakaoId,
-                googleId: res.data.googldId,
-                isAdmin: res.data.isAdmin,
-                userEmail: res.data.userEmail,
-                userName: res.data.userName,
-                token: res.data.token
-              })
-            );
-            dispatch(insertAllSticks(res.data.orders.sticks));
-            dispatch(insertAllStands(res.data.orders.stands));
-          })
-          .catch((err) => console.log('Kakao Login', err));
+        .then((res) => {
+          dispatch(
+            insertUserinfo({
+              id: res.data.id,
+              kakaoId: res.data.kakaoId,
+              googleId: res.data.googldId,
+              isAdmin: res.data.isAdmin,
+              userEmail: res.data.userEmail,
+              userName: res.data.userName,
+              token: res.data.token
+            })
+          );
+          dispatch(insertAllSticks(res.data.orders.sticks));
+          dispatch(insertAllStands(res.data.orders.stands));
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          console.log('Kakao Login', err);
+          setIsLoading(false);
+        });
       }
-    }
-  };
-  useEffect(() => {
-    setIsLoading(true);
-    didMount();
-    if (userinfo.userinfo.token !== '') {
-      axios({
-        method: 'GET',
-        url: 'https://www.cloudi.shop/auth',
-        headers: { Authorization: userinfo.userinfo.token }
-      })
+    } else {
+      if (userinfo.userinfo.token !== '') {
+        axios({
+          method: 'GET',
+          url: 'https://www.cloudi.shop/auth',
+          headers: { Authorization: userinfo.userinfo.token }
+        })
         .then((res) => {
           dispatch(insertAllStands(res.data.orders.stands));
           dispatch(insertAllSticks(res.data.orders.sticks));
@@ -102,9 +107,14 @@ const App = () => {
           dispatch(removeAllStands());
           setIsLoading(false);
         });
-    } else {
-      setIsLoading(false);
+      } else {
+        setIsLoading(false);
+      }
     }
+  };
+  
+  useEffect(() => {
+    didMount();
   }, []);  // eslint-disable-line
 
   return (
